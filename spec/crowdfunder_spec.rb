ed62a88f855077a69e8a7c20a44274bd3b83637e
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-07-24 02:56:32 +0000

    Automatically backed up by Learn

--- spec/crowdfunder_spec.rb
+++ spec/crowdfunder_spec.rb
@@ -108,7 +108,6 @@ describe "crowdfunder sql" do
     end
 
     it '#selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name' do
-      binding.pry
       expect(@db.execute(selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name)).to eq([["Iguana", 10], ["Swizzle", 12], ["Sirius", 19], ["Alex", 20], ["Marisa", 24], ["Voldemort", 34], ["Amanda", 40], ["Bear", 50], ["Hermione", 50], ["Rosey", 50], ["Pacha", 60], ["Sophie", 60], ["Finnebar", 70], ["Franz", 90], ["Ena", 100], ["Whale", 125.5], ["Katie", 170], ["Squid", 270], ["Albus", 470], ["Victoria", 1700]])
     end
 
